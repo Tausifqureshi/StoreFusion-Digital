19ef4feb3fd312af81e0006f43c5168e74fd0aea
Tausifqureshi <tausifqureshi504@gmail.com>
2024-10-07 09:16:51 +0530

    All

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,188 +1,10 @@
-
-// import { Dialog, Transition } from "@headlessui/react";
-// import { Fragment, useState } from "react";
-
-// function Modal() {
-//   // const{ fullName, address, pincode, phoneNumber } = formData
-//   // console.log("input feild", formData)
-//   let [isOpen, setIsOpen] = useState(false);
-
-//   // Modal open and close
-//   function closeModal() {
-//     setIsOpen(false);
-//   }
-
-//   function openModal() {
-//     setIsOpen(true);
-//   }
-  
-//   const handleInputChange = (e) => {
-//     setFormData({ ...formData, [e.target.name]: e.target.value });
-//     console.log(e.target.value)
-//   };
-
-
-//   return (
-//     <>
-//       <div className="text-center">
-//         <button
-//           type="button"
-//           onClick={openModal}
-//           className="w-full bg-blue-600 py-2 text-white font-bold rounded-lg hover:bg-blue-700 transition-colors"
-//         >
-//           Buy Now
-//         </button>
-//       </div>
-
-//       <Transition appear show={isOpen} as={Fragment}>
-//         <Dialog as="div" className="relative z-10" onClose={closeModal}>
-//           <Transition.Child
-//             as={Fragment}
-//             enter="ease-out duration-300"
-//             enterFrom="opacity-0"
-//             enterTo="opacity-100"
-//             leave="ease-in duration-200"
-//             leaveFrom="opacity-100"
-//             leaveTo="opacity-0"
-//           >
-//             <div className="fixed inset-0 bg-black bg-opacity-50" />
-//           </Transition.Child>
-
-//           <div className="fixed inset-0 overflow-y-auto">
-//             <div className="flex min-h-full items-start justify-center pt-28 px-4 text-center sm:items-start sm:pt-[7.1rem] md:pt-[7.1rem] lg:pt-[7.1rem]">
-//               <Transition.Child
-//                 as={Fragment}
-//                 enter="ease-out duration-300"
-//                 enterFrom="opacity-0 scale-95"
-//                 enterTo="opacity-100 scale-100"
-//                 leave="ease-in duration-200"
-//                 leaveFrom="opacity-100 scale-100"
-//                 leaveTo="opacity-0 scale-95"
-//               >
-//                 <Dialog.Panel className="w-full max-w-lg transform overflow-hidden rounded-lg bg-white p-6 shadow-xl transition-all">
-//                   <div className="flex flex-col items-center justify-center">
-//                     <h2 className="text-lg font-semibold text-gray-900 mb-4">
-//                       Complete Your Order
-//                     </h2>
-//                     <form className="w-full space-y-3">
-//                       <div>
-//                         <label
-//                           htmlFor="fullName"
-//                           className="block text-sm font-medium text-gray-700"
-//                         >
-//                           Full Name
-//                         </label>
-//                         <input
-//                           type="text"
-//                           name="fullName"
-//                           id="fullName"
-//                           className="w-full border border-gray-300 rounded-md p-2 bg-gray-100 outline-none focus:ring-blue-500 focus:border-blue-500"
-                         
-//                         />
-//                       </div>
-
-//                       <div>
-//                         <label
-//                           htmlFor="address"
-//                           className="block text-sm font-medium text-gray-700"
-//                         >
-//                           Address
-//                         </label>
-//                         <input
-//                           type="text"
-//                           name="address"
-//                           id="address"
-//                           className="w-full border border-gray-300 rounded-md p-2 bg-gray-100 outline-none focus:ring-blue-500 focus:border-blue-500"
-                        
-//                         />
-//                       </div>
-
-//                       <div>
-//                         <label
-//                           htmlFor="pincode"
-//                           className="block text-sm font-medium text-gray-700"
-//                         >
-//                           Pincode
-//                         </label>
-//                         <input
-//                           type="text"
-//                           name="pincode"
-//                           id="pincode"
-//                           className="w-full border border-gray-300 rounded-md p-2 bg-gray-100 outline-none focus:ring-blue-500 focus:border-blue-500"
-                         
-//                         />
-//                       </div>
-
-//                       <div>
-//                         <label
-//                           htmlFor="phoneNumber"
-//                           className="block text-sm font-medium text-gray-700"
-//                         >
-//                           Mobile Number
-//                         </label>
-//                         <input
-//                           type="text"
-//                           name="phoneNumber"
-//                           id="phoneNumber"
-//                           className="w-full border border-gray-300 rounded-md p-2 bg-gray-100 outline-none focus:ring-blue-500 focus:border-blue-500"
-                         
-//                         />
-//                       </div>
-//                     </form>
-
-//                     <button
-//                       onClick={closeModal}
-//                       type="button"
-//                       className="mt-5 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition-colors"
-//                     >
-//                       Place Order
-//                     </button>
-//                   </div>
-//                 </Dialog.Panel>
-//               </Transition.Child>
-//             </div>
-//           </div>
-//         </Dialog>
-//       </Transition>
-//     </>
-//   );
-// }
-
-// export default Modal;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // import { Dialog, Transition } from "@headlessui/react";
 // import { Fragment, useState } from "react";
 
 // function Modal({ formData, setFormData, buyNow }) {
 //   const { fullName, address, pincode, phoneNumber } = formData;
 //   const [formError, setFormError] = useState("");
-
-//   let [isOpen, setIsOpen] = useState(false);
+//   const [isOpen, setIsOpen] = useState(false);
 
 //   function closeModal() {
 //     setIsOpen(false);
@@ -201,8 +23,7 @@
 //       setFormError("All fields are required.");
 //       return; // Modal will not close until form is complete
 //     }
-//     // Reset error and proceed with the buyNow logic
-//     setFormError("");
+//     setFormError(""); // Reset error and proceed with the buyNow logic
 //     buyNow();
 //     closeModal();
 //   };
@@ -234,7 +55,7 @@
 //           </Transition.Child>
 
 //           <div className="fixed inset-0 overflow-y-auto">
-//             <div className="flex min-h-full items-start justify-center pt-28 px-4 text-center sm:items-start sm:pt-[7.1rem] md:pt-[7.1rem] lg:pt-[7.1rem]">
+//             <div className="flex min-h-full items-start justify-center pt-28 px-4 text-center sm:items-start sm:pt-[7.1rem]">
 //               <Transition.Child
 //                 as={Fragment}
 //                 enter="ease-out duration-300"
@@ -252,10 +73,7 @@
 
 //                     <form className="w-full space-y-3">
 //                       <div>
-//                         <label
-//                           htmlFor="fullName"
-//                           className="block text-sm font-medium text-gray-700"
-//                         >
+//                         <label htmlFor="fullName" className="block text-sm font-medium text-gray-700">
 //                           Full Name
 //                         </label>
 //                         <input
@@ -269,10 +87,7 @@
 //                       </div>
 
 //                       <div>
-//                         <label
-//                           htmlFor="address"
-//                           className="block text-sm font-medium text-gray-700"
-//                         >
+//                         <label htmlFor="address" className="block text-sm font-medium text-gray-700">
 //                           Address
 //                         </label>
 //                         <input
@@ -286,10 +101,7 @@
 //                       </div>
 
 //                       <div>
-//                         <label
-//                           htmlFor="pincode"
-//                           className="block text-sm font-medium text-gray-700"
-//                         >
+//                         <label htmlFor="pincode" className="block text-sm font-medium text-gray-700">
 //                           Pincode
 //                         </label>
 //                         <input
@@ -303,10 +115,7 @@
 //                       </div>
 
 //                       <div>
-//                         <label
-//                           htmlFor="phoneNumber"
-//                           className="block text-sm font-medium text-gray-700"
-//                         >
+//                         <label htmlFor="phoneNumber" className="block text-sm font-medium text-gray-700">
 //                           Mobile Number
 //                         </label>
 //                         <input
@@ -355,12 +164,27 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
+import { toast } from "react-toastify";
 
 function Modal({ formData, setFormData, buyNow }) {
   const { fullName, address, pincode, phoneNumber } = formData;
-  const [formError, setFormError] = useState("");
   const [isOpen, setIsOpen] = useState(false);
 
   function closeModal() {
@@ -377,12 +201,19 @@ function Modal({ formData, setFormData, buyNow }) {
 
   const handleBuyNow = () => {
     if (!fullName || !address || !pincode || !phoneNumber) {
-      setFormError("All fields are required.");
+      toast.error("All fields are required.");
       return; // Modal will not close until form is complete
     }
-    setFormError(""); // Reset error and proceed with the buyNow logic
+
     buyNow();
     closeModal();
+    // Reset the formData after successful submission
+    setFormData({
+      fullName: '',
+      address: '',
+      pincode: '',
+      phoneNumber: '',
+    });
   };
 
   return (
@@ -486,10 +317,6 @@ function Modal({ formData, setFormData, buyNow }) {
                       </div>
                     </form>
 
-                    {formError && (
-                      <p className="text-red-500 mt-2">{formError}</p>
-                    )}
-
                     <button
                       onClick={handleBuyNow}
                       type="button"
@@ -509,185 +336,3 @@ function Modal({ formData, setFormData, buyNow }) {
 }
 
 export default Modal;
-
-
-
-
-
-
-
-
-
-
-// import { Dialog, Transition } from "@headlessui/react";
-// import { Fragment, useState } from "react";
-// import { toast } from "react-toastify";
-
-// function Modal({ formData, setFormData, buyNow }) {
-//   const { fullName, address, pincode, phoneNumber } = formData;
-//   let [isOpen, setIsOpen] = useState(false);
-
-//   function closeModal() {
-//     setIsOpen(false);
-//   }
-
-//   function openModal() {
-//     setIsOpen(true);
-//   }
-
-//   const handleInputChange = (e) => {
-//     setFormData({ ...formData, [e.target.name]: e.target.value });
-//   };
-
-//   const handleBuyNow = () => {
-//     // Check if any field is missing, show toast error if required
-//     if (!fullName || !address || !pincode || !phoneNumber) {
-//       toast.error("All fields are required", {
-//         position: "top-center",
-//         autoClose: 1200,
-//         hideProgressBar: false,
-//         closeOnClick: true,
-//         pauseOnHover: true,
-//         draggable: true,
-//         theme: "colored",
-//       });
-//       return;
-//     }
-
-//     // Proceed to buy now logic
-//     buyNow();
-//     closeModal();
-//   };
-
-//   return (
-//     <>
-//       <div className="text-center">
-//         <button
-//           type="button"
-//           onClick={openModal}
-//           className="w-full bg-blue-600 py-2 text-white font-bold rounded-lg hover:bg-blue-700 transition-colors"
-//         >
-//           Buy Now
-//         </button>
-//       </div>
-
-//       <Transition appear show={isOpen} as={Fragment}>
-//         <Dialog as="div" className="relative z-10" onClose={closeModal}>
-//           <Transition.Child
-//             as={Fragment}
-//             enter="ease-out duration-300"
-//             enterFrom="opacity-0"
-//             enterTo="opacity-100"
-//             leave="ease-in duration-200"
-//             leaveFrom="opacity-100"
-//             leaveTo="opacity-0"
-//           >
-//             <div className="fixed inset-0 bg-black bg-opacity-50" />
-//           </Transition.Child>
-
-//           <div className="fixed inset-0 overflow-y-auto">
-//             <div className="flex min-h-full items-start justify-center pt-28 px-4 text-center sm:items-start sm:pt-[7.1rem] md:pt-[7.1rem] lg:pt-[7.1rem]">
-//               <Transition.Child
-//                 as={Fragment}
-//                 enter="ease-out duration-300"
-//                 enterFrom="opacity-0 scale-95"
-//                 enterTo="opacity-100 scale-100"
-//                 leave="ease-in duration-200"
-//                 leaveFrom="opacity-100 scale-100"
-//                 leaveTo="opacity-0 scale-95"
-//               >
-//                 <Dialog.Panel className="w-full max-w-lg transform overflow-hidden rounded-lg bg-white p-6 shadow-xl transition-all">
-//                   <div className="flex flex-col items-center justify-center">
-//                     <h2 className="text-lg font-semibold text-gray-900 mb-4">
-//                       Complete Your Order
-//                     </h2>
-
-//                     <form className="w-full space-y-3">
-//                       <div>
-//                         <label
-//                           htmlFor="fullName"
-//                           className="block text-sm font-medium text-gray-700"
-//                         >
-//                           Full Name
-//                         </label>
-//                         <input
-//                           type="text"
-//                           name="fullName"
-//                           id="fullName"
-//                           className="w-full border border-gray-300 rounded-md p-2 bg-gray-100 outline-none focus:ring-blue-500 focus:border-blue-500"
-//                           value={fullName}
-//                           onChange={handleInputChange}
-//                         />
-//                       </div>
-
-//                       <div>
-//                         <label
-//                           htmlFor="address"
-//                           className="block text-sm font-medium text-gray-700"
-//                         >
-//                           Address
-//                         </label>
-//                         <input
-//                           type="text"
-//                           name="address"
-//                           id="address"
-//                           className="w-full border border-gray-300 rounded-md p-2 bg-gray-100 outline-none focus:ring-blue-500 focus:border-blue-500"
-//                           value={address}
-//                           onChange={handleInputChange}
-//                         />
-//                       </div>
-
-//                       <div>
-//                         <label
-//                           htmlFor="pincode"
-//                           className="block text-sm font-medium text-gray-700"
-//                         >
-//                           Pincode
-//                         </label>
-//                         <input
-//                           type="text"
-//                           name="pincode"
-//                           id="pincode"
-//                           className="w-full border border-gray-300 rounded-md p-2 bg-gray-100 outline-none focus:ring-blue-500 focus:border-blue-500"
-//                           value={pincode}
-//                           onChange={handleInputChange}
-//                         />
-//                       </div>
-
-//                       <div>
-//                         <label
-//                           htmlFor="phoneNumber"
-//                           className="block text-sm font-medium text-gray-700"
-//                         >
-//                           Mobile Number
-//                         </label>
-//                         <input
-//                           type="text"
-//                           name="phoneNumber"
-//                           id="phoneNumber"
-//                           className="w-full border border-gray-300 rounded-md p-2 bg-gray-100 outline-none focus:ring-blue-500 focus:border-blue-500"
-//                           value={phoneNumber}
-//                           onChange={handleInputChange}
-//                         />
-//                       </div>
-//                     </form>
-
-//                     <button
-//                       onClick={handleBuyNow}
-//                       type="button"
-//                       className="mt-5 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition-colors"
-//                     >
-//                       Place Order
-//                     </button>
-//                   </div>
-//                 </Dialog.Panel>
-//               </Transition.Child>
-//             </div>
-//           </div>
-//         </Dialog>
-//       </Transition>
-//     </>
-//   );
-// }
-
-// export default Modal;
--- src/pages/razorpay/Razorpay.jsx
+++ src/pages/razorpay/Razorpay.jsx
@@ -1,82 +1,4 @@
 
-// import React, { useState } from "react";
-// import Modal from "../../components/modal/Modal";
-// import { addDoc, collection } from "firebase/firestore";
-// import { fireDB } from "../../firebase/FirebaseConfig";
-// import { toast } from "react-toastify";
-
-// function Razorpay() {
-//   const [formData, setFormData] = useState({ fullName: '', address: '', pincode: '', phoneNumber: '' });
-
-//   const buyNow = async () => {
-//     const addressInfo = {
-//       name: formData.fullName,
-//       address: formData.address,
-//       pincode: formData.pincode,
-//       phoneNumber: formData.phoneNumber,
-//       date: new Date().toLocaleString("en-US", {
-//         month: "short",
-//         day: "2-digit",
-//         year: "numeric",
-//       }),
-//     };
-
-//     var options = {
-//       key: "",
-//       key_secret: "",
-//       amount: parseInt(1000 * 100), // Assuming `grandTotal` as 1000 for now
-//       currency: "INR",
-//       order_receipt: 'order_rcptid_' + formData.fullName,
-//       name: "E-Bharat",
-//       description: "for testing purpose",
-//       handler: async function (response) {
-//         toast.success('Payment Successful');
-
-//         const paymentId = response.razorpay_payment_id;
-//         const orderInfo = {
-//           addressInfo,
-//           date: new Date().toLocaleString("en-US", {
-//             month: "short",
-//             day: "2-digit",
-//             year: "numeric",
-//           }),
-//         //   email: JSON.parse(localStorage.getItem("user")).user.email,
-//         //   userid: JSON.parse(localStorage.getItem("user")).user.uid,
-//         //   paymentId,
-//         };
-
-//         try {
-//           await addDoc(collection(fireDB, "orders"), orderInfo);
-//         } catch (error) {
-//           console.log(error);
-//         }
-//       },
-//       theme: {
-//         color: "#3399cc",
-//       },
-//     };
-
-//     var pay = new window.Razorpay(options);
-//     pay.open();
-//   };
-
-//   return (
-//     <div>
-//       {/* Passing the buyNow function and formData state to Modal */}
-//       <Modal buyNow={buyNow} formData={formData} setFormData={setFormData} />
-//     </div>
-//   );
-// }
-
-// export default Razorpay;
-
-
-
-
-
-
-
-
 import React, { useState } from "react";
 import Modal from "../../components/modal/Modal";
 import { addDoc, collection } from "firebase/firestore";
@@ -84,12 +6,7 @@ import { fireDB } from "../../firebase/FirebaseConfig";
 import { toast } from "react-toastify";
 
 function Razorpay() {
-  const [formData, setFormData] = useState({
-    fullName: '',
-    address: '',
-    pincode: '',
-    phoneNumber: ''
-  });
+  const [formData, setFormData] = useState({ fullName: '', address: '', pincode: '', phoneNumber: '' });
 
   const buyNow = async () => {
     const addressInfo = {
@@ -105,18 +22,17 @@ function Razorpay() {
     };
 
     var options = {
-      key: "YOUR_TEST_KEY", // Test API Key
-      amount: parseInt(1000 * 100), // Replace with your grandTotal variable
+      key: "rzp_test_s2VG2G2HwcOQd6",
+      key_secret: "13wTYUM144Kv98GujKu6kkB6",
+      amount: parseInt(1000 * 100), // Assuming `grandTotal` as 1000 for now
       currency: "INR",
       order_receipt: 'order_rcptid_' + formData.fullName,
-      name: "E-Bharat",
+      name: "StoreFusion",
       description: "for testing purpose",
       handler: async function (response) {
-        const paymentId = response.razorpay_payment_id;
-
-        // Success message
         toast.success('Payment Successful');
 
+        const paymentId = response.razorpay_payment_id;
         const orderInfo = {
           addressInfo,
           date: new Date().toLocaleString("en-US", {
@@ -124,15 +40,15 @@ function Razorpay() {
             day: "2-digit",
             year: "numeric",
           }),
+          email: JSON.parse(localStorage.getItem("user")).user.email,
+          userid: JSON.parse(localStorage.getItem("user")).user.uid,
           paymentId,
         };
 
         try {
           await addDoc(collection(fireDB, "orders"), orderInfo);
-          toast.success('Order placed successfully!');
         } catch (error) {
-          console.error(error);
-          toast.error('Error while placing order');
+          console.log(error);
         }
       },
       theme: {
@@ -153,3 +69,87 @@ function Razorpay() {
 }
 
 export default Razorpay;
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import Modal from "../../components/modal/Modal";
+// import { addDoc, collection } from "firebase/firestore";
+// import { fireDB } from "../../firebase/FirebaseConfig";
+// import { toast } from "react-toastify";
+
+// function Razorpay() {
+//   const [formData, setFormData] = useState({
+//     fullName: '',
+//     address: '',
+//     pincode: '',
+//     phoneNumber: ''
+//   });
+
+//   const buyNow = async () => {
+//     const addressInfo = {
+//       name: formData.fullName,
+//       address: formData.address,
+//       pincode: formData.pincode,
+//       phoneNumber: formData.phoneNumber,
+//       date: new Date().toLocaleString("en-US", {
+//         month: "short",
+//         day: "2-digit",
+//         year: "numeric",
+//       }),
+//     };
+
+//     var options = {
+//       key: "YOUR_TEST_KEY", // Test API Key
+//       amount: parseInt(1000 * 100), // Replace with your grandTotal variable
+//       currency: "INR",
+//       order_receipt: 'order_rcptid_' + formData.fullName,
+//       name: "E-Bharat",
+//       description: "for testing purpose",
+//       handler: async function (response) {
+//         const paymentId = response.razorpay_payment_id;
+
+//         // Success message
+//         toast.success('Payment Successful');
+
+//         const orderInfo = {
+//           addressInfo,
+//           date: new Date().toLocaleString("en-US", {
+//             month: "short",
+//             day: "2-digit",
+//             year: "numeric",
+//           }),
+//           paymentId,
+//         };
+
+//         try {
+//           await addDoc(collection(fireDB, "orders"), orderInfo);
+//           toast.success('Order placed successfully!');
+//         } catch (error) {
+//           console.error(error);
+//           toast.error('Error while placing order');
+//         }
+//       },
+//       theme: {
+//         color: "#3399cc",
+//       },
+//     };
+
+//     var pay = new window.Razorpay(options);
+//     pay.open();
+//   };
+
+//   return (
+//     <div>
+//       {/* Passing the buyNow function and formData state to Modal */}
+//       <Modal buyNow={buyNow} formData={formData} setFormData={setFormData} />
+//     </div>
+//   );
+// }
+
+// export default Razorpay;
