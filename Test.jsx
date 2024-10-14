e52f2cb962e2c17b35ded378eb182615ad9a7be7
Tausifqureshi <tausifqureshi504@gmail.com>
2024-10-14 23:16:22 +0530

    All

--- Test.jsx
+++ Test.jsx
@@ -110,66 +110,6 @@
 
 
 
-## 🛒 StoreFusion-Digital Clone
-
-The **StoreFusion-Digital Clone** is a scalable e-commerce platform offering innovative, seamless digital retail solutions to enhance user experience and drive growth.
-
----
-
-## 🌐 Live Preview
-
-[**Click here** to view the live preview of the StoreFusion-Digital Clone project](https://storefusion-digital.netlify.app/).
-
----
-
-## 🚀 Features
-
-- **Sign-Up, Login Page**: Secure authentication for users.
-- **Product Page**: Display a variety of products with filtering and sorting options.
-- **Admin Dashboard**: Manage products, orders, and user data from an intuitive dashboard.
-- **Cart Page**: Add products to the cart and update quantities.
-- **Payment Gateway**: Integrated payment system for seamless transactions.
-
----
-
-## 🛠️ Technologies Used
-
-- **React**
-- **React Router**
-- **React Redux**
-- **Tailwind CSS**
-- **Material-UI**
-- **Firebase**
-- **Firebase Firestore**
-
----
-
-## 📚 Getting Started
-
-To run the project locally, follow these steps:
-
-1. **Clone the repository**:
-   ```bash
-   git clone https://github.com/Tausifqureshi/StoreFusion-Digital
-
-
-2. **Go to the project directory**
-```bash
-cd Udemy-Clone
-```
-
-3. **Install the dependencies**
-```bash
-npm install
-```
-
-4. **Start the development server**
-```bash
-npm start
-```
-
-
-
 
 
 
