c91b0053b8cacf19ff8717c5dd6a3145548665f9
Tausifqureshi <tausifqureshi504@gmail.com>
2026-03-17 11:43:03 +0530

    Updated now ProductInfo

--- src/pages/productInfo/ProductInfo.jsx
+++ src/pages/productInfo/ProductInfo.jsx
@@ -1,28 +1,28 @@
-// import React, { useContext, useState, useEffect } from 'react';
-// import Layout from '../../components/layout/Layout';
-// import { toast } from 'react-toastify';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { addToCart } from '../../redux/cartSlice';
-// import { fireDB } from '../../firebase/FirebaseConfig';
-// import { useParams, Link  } from 'react-router-dom';
-// import { doc, getDoc } from 'firebase/firestore';
-// import { MyContext } from '../../context api/myContext';
-// import Loader from '../../components/loader/Loader';
-// import { FaHeart, FaShoppingCart, FaTwitter, FaInstagram, FaYoutube } from 'react-icons/fa';
+// // import React, { useContext, useState, useEffect } from 'react';
+// // import Layout from '../../components/layout/Layout';
+// // import { toast } from 'react-toastify';
+// // import { useDispatch, useSelector } from 'react-redux';
+// // import { addToCart } from '../../redux/cartSlice';
+// // import { fireDB } from '../../firebase/FirebaseConfig';
+// // import { useParams, Link  } from 'react-router-dom';
+// // import { doc, getDoc } from 'firebase/firestore';
+// // import { MyContext } from '../../context api/myContext';
+// // import Loader from '../../components/loader/Loader';
+// // import { FaHeart, FaShoppingCart, FaTwitter, FaInstagram, FaYoutube } from 'react-icons/fa';
 
-// const StarIcon = ({ filled = true }) => (
-//     <svg
-//         fill={filled ? "currentColor" : "none"}
-//         stroke="currentColor"
-//         strokeLinecap="round"
-//         strokeLinejoin="round"
-//         strokeWidth={2}
-//         className="w-5 h-5 text-indigo-500"
-//         viewBox="0 0 24 24"
-//     >
-//         <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-//     </svg>
-// );
+// // const StarIcon = ({ filled = true }) => (
+// //     <svg
+// //         fill={filled ? "currentColor" : "none"}
+// //         stroke="currentColor"
+// //         strokeLinecap="round"
+// //         strokeLinejoin="round"
+// //         strokeWidth={2}
+// //         className="w-5 h-5 text-indigo-500"
+// //         viewBox="0 0 24 24"
+// //     >
+// //         <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
+// //     </svg>
+// // );
 
 // function ProductInfo() {
 //     const { loading, setLoading } = useContext(MyContext);
